31f25576255e5e65541c617a10a3e2a247fe7459
Alex Bejan <alex_bejan11@yahoo.com>
2022-04-07 11:40:20 +0300

    Base start for api and added graphql client for flutter frontend

---
+++ api/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './schemas/index';
+import resolvers from './resolvers/index';
+import dotenv from 'dotenv';
+import connectMongo from './db/db';
+
+dotenv.config();
+
+(async () => {
+    try {
+        const conn = await connectMongo();
+        if (conn) {
+            console.log('Connected successfully.');
+        } else {
+            throw new Error('db not connected');
+        }
+
+        const server = new ApolloServer({
+            typeDefs,
+            resolvers,
+        });
+
+        const app = express();
+
+        await server.start();
+
+        server.applyMiddleware({ app });
+
+        app.listen({ port: process.env.PORT || 3000 }, () =>
+            console.log(
+                `🚀 Server ready at http://localhost:3000${server.graphqlPath}`
+            )
+        );
+    } catch (e) {
+        console.log('server error: ' + e.message);
+    }
+})();
---
+++ api/start.js
@@ -0,0 +1,4 @@
+// Set options as a parameter, environment variable, or rc file.
+console.log('that should start the app?');
+require = require("esm")(module/*, options*/)
+module.exports = require("./app.js")
