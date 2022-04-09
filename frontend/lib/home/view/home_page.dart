bdaf480d24d5c021c266ad4e4ad3ecc767b5d87e
Alex Bejan <alex_bejan11@yahoo.com>
2022-04-09 10:46:51 +0300

    Added bottom modal sheet for adding, renaming and deleting areas

--- frontend/lib/home/view/home_page.dart
+++ frontend/lib/home/view/home_page.dart
@@ -26,17 +26,62 @@ class HomePage extends StatelessWidget {
 class CounterView extends StatelessWidget {
   const CounterView({Key? key}) : super(key: key);
 
+  Future<void> _settingModalBottomSheet(BuildContext context) async {
+    await showModalBottomSheet<dynamic>(
+      context: context,
+      builder: (BuildContext bc) {
+        return Wrap(
+          children: <Widget>[
+            ListTile(
+              title: const Text('Add area'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Rename area'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Delete area'),
+              onTap: () {},
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            print("text");
-          },
+          backgroundColor: Theme.of(context).bottomAppBarColor,
+          onPressed: () {},
+          child: const Icon(
+            Icons.add,
+          ),
+        ),
+        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+        bottomNavigationBar: BottomAppBar(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          shape: const CircularNotchedRectangle(),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () async {
+                    await _settingModalBottomSheet(context);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
         appBar: AppBar(
           title: const Text('Tabs Demo'),
