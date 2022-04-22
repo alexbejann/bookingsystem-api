9700ad7274cf7f7810f1ad48c3ada3d840431cee
Alex Bejan <alex_bejan11@yahoo.com>
2022-04-22 14:34:15 +0300

    Implemented resolver and schema for workspace

--- api/resolvers/workspaceResolver.js
+++ api/resolvers/workspaceResolver.js
@@ -1,11 +1,34 @@
 'use strict';
-
+import Workspace from '../models/workspace';
 
 export default {
     Query: {
-
+        workspaces: async (parent, args) => {
+            console.log('workspaces', parent, args);
+            return await Workspace.find({officeId: args.officeID});
+        }
     },
     Mutation: {
-
+        addWorkspace: async (parent, args) => {
+            console.log('addWorkspace', parent, args);
+            const newWorkspace = new Workspace(args);
+            console.log('newWorkspace', newWorkspace);
+            return await newWorkspace.save();
+        },
+        renameWorkspace: async (parent, args) => {
+            console.log('renameWorkspace', parent, args);
+            return await Workspace
+                .findByIdAndUpdate(args.workspaceID,
+                    {
+                        name: args.newName
+                    },
+                    {
+                        new: true,
+                    });
+        },
+        deleteWorkspace: async (parent, args) => {
+            console.log('deleteWorkspace', parent, args);
+            return await Workspace.findOneAndDelete({_id: args.id});
+        }
     },
 };
--- api/schemas/workspaceSchema.js
+++ api/schemas/workspaceSchema.js
@@ -3,17 +3,18 @@ import {gql} from 'apollo-server-express';
 
 export default gql`
   extend type Query {
-    workspaces(organizationID : ID!): [Workspace]
+    workspaces(officeID : ID!): [Workspace]
   }
   
   extend type Mutation {
-    addWorkspace(name: String!): Workspace
-    renameOffice(name: String!): Workspace
-    deleteOffice(id: ID!): Workspace
+    addWorkspace(name: String!, officeID : ID!): Workspace
+    renameWorkspace(newName: String!, workspaceID: ID!): Workspace
+    deleteWorkspace(id: ID!): Workspace
   }
   
   type Workspace {
     id: ID
     name: String
+    officeID: ID
   }
 `;
