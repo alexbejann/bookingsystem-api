440f1891eaf7120b8defb6537fb1362aaf346a7b
Alex Bejan <alex_bejan11@yahoo.com>
2022-04-22 12:00:35 +0300

    Added booking bloc and page view

--- frontend/lib/bookings/view/my_bookings_page.dart
+++ frontend/lib/bookings/view/my_bookings_page.dart
@@ -1,11 +1,31 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/bookings/bloc/booking_bloc.dart';
 
 class MyBookingsPage extends StatelessWidget {
   const MyBookingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => BookingBloc(),
+      child: MyBookingsView(),
+    );
+  }
+}
+
+class MyBookingsView extends StatefulWidget {
+  const MyBookingsView({Key? key}) : super(key: key);
+
+  @override
+  State<MyBookingsView> createState() => _MyBookingsViewState();
+}
+
+class _MyBookingsViewState extends State<MyBookingsView> {
+  @override
+  Widget build(BuildContext context) {
     return const Center(child: Text('My Bookings'),);
   }
 }
+
