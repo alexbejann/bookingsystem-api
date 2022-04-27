bdb4bc1d1c595c823ed0a4949350b21865fa638e
Alex Bejan <alex_bejan11@yahoo.com>
2022-04-27 23:46:55 +0300

    Added timeslot bloc events

--- frontend/lib/timeslots/bloc/timeslot_event.dart
+++ frontend/lib/timeslots/bloc/timeslot_event.dart
@@ -1,5 +1,19 @@
 part of 'timeslot_bloc.dart';
 
 @immutable
-abstract class TimeslotEvent {}
+abstract class TimeslotEvent {
+  const TimeslotEvent();
+}
+
+class GetTimeslots extends TimeslotEvent {
+  const GetTimeslots(this.workspaceId);
+
+  final String workspaceId;
+}
+
+class DeleteTimeslot extends TimeslotEvent {
+  const DeleteTimeslot(this.timeslotId);
+
+  final String timeslotId;
+}
 
