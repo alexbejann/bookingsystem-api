cd37d8f0b5649bf7f95c0a193b899c79469daf3e
Alex Bejan <alex_bejan11@yahoo.com>
2022-04-27 23:37:23 +0300

    Added most of the needed mutations

--- frontend/lib/utils/graphql/mutations.dart
+++ frontend/lib/utils/graphql/mutations.dart
@@ -18,3 +18,82 @@ mutation Mutation($name: String!, $organizationId: ID!) {
   }
 }
 ''';
+
+const String renameOffice = r'''
+mutation Mutation($newName: String!, $officeId: ID!) {
+  renameOffice(newName: $newName, officeID: $officeId) {
+    id
+    name
+    organizationID {
+      id
+    }
+  }
+}
+''';
+
+const String deleteOffice = r'''
+mutation Mutation($deleteOfficeId: ID!) {
+  deleteOffice(id: $deleteOfficeId) {
+    id
+  }
+}
+''';
+
+const String addTimeslot = r'''
+mutation Mutation($title: String!, $from: DateTime!, $to: DateTime!, $userId: ID, $workspaceId: ID) {
+  addTimeslot(title: $title, from: $from, to: $to, userID: $userId, workspaceID: $workspaceId) {
+    id
+    title
+    from
+    to
+    userID {
+      id
+    }
+  }
+}
+mutation DeleteWorkspace($timeslotId: ID!) {
+  removeTimeslot(timeslotID: $timeslotId) {
+    id
+  }
+}
+''';
+
+const String removeTimeslot = r'''
+mutation Mutation($timeslotId: ID!) {
+  removeTimeslot(timeslotID: $timeslotId) {
+    id
+  }
+}
+''';
+
+const String addWorkspace = r'''
+mutation Mutation($name: String!, $officeId: ID!) {
+  addWorkspace(name: $name, officeID: $officeId) {
+    id
+    name
+    officeID {
+      id
+    }
+  }
+}
+''';
+
+const String renameWorkspace = r'''
+mutation Mutation($newName: String!, $workspaceId: ID!) {
+  renameWorkspace(newName: $newName, workspaceID: $workspaceId) {
+    id
+    name
+    officeID {
+      id
+    }
+  }
+}
+''';
+
+const String deleteWorkspace = r'''
+mutation Mutation($deleteWorkspaceId: ID!) {
+  deleteWorkspace(id: $deleteWorkspaceId) {
+    id
+  }
+}
+''';
